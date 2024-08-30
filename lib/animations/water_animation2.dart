4602b386a77166bc734b1a17d957e5213e9c6b59
unknown <nhd7noushadazeez@gmail.com>
2024-08-30 15:36:50 +0530

    added water animations

---
+++ lib/animations/water_animation2.dart
@@ -0,0 +1,135 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class WaveProgress extends StatefulWidget {
+  final double size;
+  final Color borderColor, fillColor;
+  final double progress;
+
+  const WaveProgress(this.size, this.borderColor, this.fillColor, this.progress,
+      {super.key});
+
+  @override
+  WaveProgressState createState() => WaveProgressState();
+}
+
+class WaveProgressState extends State<WaveProgress>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2500),
+    );
+
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: widget.size,
+        height: widget.size,
+        //decoration: new BoxDecoration(color: Colors.green),
+        child: AnimatedBuilder(
+            animation: controller,
+            builder: (BuildContext context, Widget? child) {
+              return CustomPaint(
+                  painter: WaveProgressPainter(controller, widget.borderColor,
+                      widget.fillColor, widget.progress));
+            }));
+  }
+}
+
+class WaveProgressPainter extends CustomPainter {
+  final Animation<double> _animation;
+  Color borderColor, fillColor;
+  final double _progress;
+
+  WaveProgressPainter(
+      this._animation, this.borderColor, this.fillColor, this._progress)
+      : super(repaint: _animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // draw small wave
+    Paint wave2Paint = Paint()..color = fillColor.withOpacity(0.5);
+    double p = _progress / 100.0;
+    double n = 4.2;
+    double amp = 4.0;
+    double baseHeight = (1 - p) * size.height;
+
+    Path path = Path();
+    path.moveTo(0.0, baseHeight);
+    for (double i = 0.0; i < size.width; i++) {
+      path.lineTo(
+          i,
+          baseHeight +
+              sin((i / size.width * 2 * pi * n) +
+                      (_animation.value * 2 * pi) +
+                      pi * 1) *
+                  amp);
+    }
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.close();
+    canvas.drawPath(path, wave2Paint);
+
+    // draw big wave
+    Paint wave1Paint = Paint()..color = fillColor;
+    n = 2.2;
+    amp = 10.0;
+
+    path = Path();
+    path.moveTo(0.0, baseHeight);
+    for (double i = 0.0; i < size.width; i++) {
+      path.lineTo(
+          i,
+          baseHeight +
+              sin((i / size.width * 2 * pi * n) + (_animation.value * 2 * pi)) *
+                  amp);
+    }
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.close();
+    canvas.drawPath(path, wave1Paint);
+
+    // draw border
+    // Paint borderPaint = Paint()
+    //   ..color = borderColor
+    //   ..style = PaintingStyle.stroke
+    //   ..strokeWidth = 10.0;
+
+    // canvas.drawCircle(size.center(Offset.zero), size.width / 2, borderPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class CircleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..addOval(Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2),
+          radius: size.width / 2));
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animations/animations/brick_animation.dart';
-import 'package:flutter_animations/animations/leaf_ui/leaf_details.dart';
-import 'package:flutter_animations/animations/round_slider.dart';
-import 'package:flutter_animations/animations/switch_lock.dart';
-import 'package:flutter_animations/animations/text_animation/typing_animation.dart';
-import 'package:flutter_animations/animations/text_animation/wave_animation.dart';
-import 'package:flutter_animations/clippers/tickets.dart';
-import 'package:flutter_animations/custom_paint/cat.dart';
-import 'package:flutter_animations/custom_paint/chameleon.dart';
-import 'package:flutter_animations/custom_paint/dash_design.dart';
-import 'package:flutter_animations/custom_paint/frog_head.dart';
-import 'package:flutter_animations/custom_paint/phone_pattern.dart';
-import 'package:flutter_animations/custom_paint/pikachu.dart';
-import 'package:flutter_animations/loading_animations/spin_loading.dart';
-import 'package:flutter_animations/onboarding/onboarding.dart';
+import 'package:flutter_animations/animations/water_animation.dart';
+import 'package:flutter_animations/animations/water_animation2.dart';
+import 'package:flutter_animations/animations/wave_animation.dart';
+import 'package:flutter_animations/animations/wave_water_animation.dart';
 import 'package:flutter_animations/state_management/example/color_nobot.dart';
 import 'package:flutter_animations/state_management/example/counter_nobot.dart';
-import 'package:flutter_animations/state_management/example/counter_view.dart';
-import 'package:flutter_animations/state_management/nobot_provider.dart';
-import 'package:provider/provider.dart';
 
 import 'animations/leaf_ui/leaf_screen.dart';
-import 'animations/zoom_drawer/provider/page_provider.dart';
-import 'animations/zoom_drawer/screens/drawer_animation.dart';
+import 'state_management/nobot_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -41,7 +26,12 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
             debugShowCheckedModeBanner: false,
-            home: const LeafScreen(),
+            home: WaveProgress(
+              MediaQuery.of(context).size.height,
+              Colors.white,
+              Colors.blue,
+              30,
+            ),
           ),
         ));
   }
